a561c9e2c218c6a9c2783f006e8e9159778dc8b3
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2023-12-16 22:55:54 +0800

    init project
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ tc_capture.h
@@ -0,0 +1,18 @@
+//
+// Created by RGAA on 2023-12-16.
+//
+
+#ifndef TC_CAPTURE_TCCAPTURE_H
+#define TC_CAPTURE_TCCAPTURE_H
+
+
+namespace tc
+{
+
+    class TCCapture {
+
+    };
+
+}
+
+#endif //TC_CAPTURE_TCCAPTURE_H

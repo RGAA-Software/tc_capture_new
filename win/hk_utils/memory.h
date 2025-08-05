8a7681494bb267b2003413f62f0b910217ad9bc0
hy <hy@gmail.com>
2025-08-05 16:17:10 +0800

    update
    Signed-off-by: hy <hy@gmail.com>

--- win/hk_utils/memory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace tc
-{
-    static inline void *GetVTableFunctionAddress(void *class_ptr, size_t offset) {
-        void **vtable = *(void ***) class_ptr;
-        return vtable[offset];
-    }
-}
--- win/hk_video/capture_dxgi_d3d11on12.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace tc_capture_d3d11on12
-{
-    void Capture(void *swap, void* back_buffer);
-    void Free();
-}
